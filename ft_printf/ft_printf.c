d25431c935ba8af4efccb28322a8ea992f6ac1bc
brel-baz <brel-baz@student.42.fr>
2017-08-23 06:14:45 +0200

    2nd commit printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,11 +6,19 @@
 /*   By: brel-baz <brel-baz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 05:11:15 by brel-baz          #+#    #+#             */
-/*   Updated: 2017/08/22 07:31:04 by brel-baz         ###   ########.fr       */
+/*   Updated: 2017/08/23 06:03:59 by brel-baz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putnbr_base(unsigned int nbr, int base, char *cbase)
+{
+	if (nbr == 0)
+		return;
+	ft_putnbr_base(nbr / base, base, cbase);
+	ft_putchar(cbase[nbr % base]);
+}
 
 int		ft_printf(const char *format, ...)
 {
@@ -24,20 +32,50 @@ int		ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '%')
+				ft_putchar('%');
+
 			if (format[i + 1] == 'c')
 			{
 				id.c = (char)va_arg(ap, int);
 				ft_putchar(id.c);
 			}
-			if (format[i + 1] == 'd')
+
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
 			{
 				id.d = va_arg(ap, int);
 				ft_putnbr(id.d);
+			}
+			if (format[i + 1] == 'u')
+			{
+				id.u = (unsigned int)va_arg(ap, int);
+				ft_putnbr(id.u);
 			}
 			if (format[i + 1] == 's')
 			{
 				id.s = va_arg(ap, char*);
 				ft_putstr(id.s);
+			}
+			if (format[i + 1] == 'o')
+			{
+				id.o = va_arg(ap, unsigned int);
+				ft_putnbr_base(id.o, 8, BASE);
+			}
+			if (format[i + 1] == 'x')
+			{
+				id.x = va_arg(ap, unsigned int);
+				ft_putnbr_base(id.x, 16, BASE);
+			}
+			if (format[i + 1] == 'X')
+			{
+				id.X = va_arg(ap, unsigned int);
+				ft_putnbr_base(id.X, 16, BASE_UP);
+			}
+			if (format[i + 1] == 'p')
+			{
+				id.p = va_arg(ap, unsigned int);
+				ft_putstr("0x");
+				ft_putnbr_base(id.p, 16, BASE);
 			}
 			i++;
 		}
@@ -49,19 +87,21 @@ int		ft_printf(const char *format, ...)
 	return (0);
 }
 
-// int 	main()
-// {
-// 	int i;
-// 	int j;
-// 	int k;
-// 	char c = 'Z';
-// 	char str[5] = "test";
-//
-// 	i = 42;
-// 	j = 90;
-// 	k = 78;
-// 	// char c = 'N';
-// 	// printf("%c\n", c);
-// 	ft_printf("%c\n", c);
-// 	return (0);
-// }
+int 	main()
+{
+	int i;
+	int j;
+	int k;
+	char c = 'Z';
+	char str[5] = "test";
+
+	i = 420;
+	j = 90;
+	k = 78;
+	printf("printf1 = %p\n", &str);
+	ft_printf("ft_printf = %p\n", &str);
+	ft_putchar('\n');
+	printf("printf i = %d , j = %d , k = %d, %s , %% , %c\n", i,j,k, str,c);
+	ft_printf("ft_printf i = %d , j = %d , k = %d, %s , %%, %c\n", i,j,k, str,c);
+	return (0);
+}
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: brel-baz <brel-baz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 03:13:37 by brel-baz          #+#    #+#             */
-/*   Updated: 2017/08/22 04:31:10 by brel-baz         ###   ########.fr       */
+/*   Updated: 2017/08/23 05:54:47 by brel-baz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 # include "libft/libft.h"
 # include <stdio.h>
 # include <stdarg.h>
-
+# define BASE "0123456789abcdef"
+# define BASE_UP "0123456789ABCDEF"
 union my_id
 {
 	char c;
 	int	d;
 	char *s;
+	unsigned int u;
+	unsigned int o;
+	unsigned int x;
+	unsigned int X;
+	unsigned int p;
 };
 #endif
